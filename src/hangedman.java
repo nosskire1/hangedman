c2593495e3c2d7c1d82db1bdc136422eea534724
nosskire1 <77719269+nosskire1@users.noreply.github.com>
2022-01-31 10:10:30 +0100

    Initial commit

---
+++ src/hangedman.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class hangedman {
+    public static void main(String[] args) {
+        String o;
+        o = JOptionPane.showInputDialog(null, "välj ditt ord");{
+
+
+            String x=0;
+
+            JOptionPane.showInputDialog(null, "x");
+        }
+
+
+    }
+
+}
